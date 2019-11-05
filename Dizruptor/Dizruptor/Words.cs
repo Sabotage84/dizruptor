3b22b72873c8901e23f8f766daba5c410c56780e
Sabotage84 <sabotage84@mail.ru>
2019-11-05 16:53:58 +0300

    ADD targetDic

--- Dizruptor/Dizruptor/Words.cs
+++ Dizruptor/Dizruptor/Words.cs
@@ -12,6 +12,7 @@ namespace Dizruptor
         private static readonly Words instance = new Words();
         public SortedDictionary<string, int> allWordsDic;
         public SortedList<string, int> badWords= new SortedList<string, int>();
+	public SortedList<string, List<string>> targetList= new SortedList<string, List<string>>();
 
         Words()
         {
